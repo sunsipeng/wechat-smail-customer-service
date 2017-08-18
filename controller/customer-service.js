47ea7d0b3218419ff159b9e3b8f3d282c96fb959
CN\sunsipeng <1163260351@qq.com>
2017-08-18 17:34:06 +0800

    接受用户消息

--- controller/customer-service.js
+++ controller/customer-service.js
@@ -1,10 +1,24 @@
+var assignReq = require('../common/isWechatRequest');
+
+exports.verification = function(req,res,next){
+	if (assignReq.isWechatRequest) {
+		console.log('wechat server request~');
+		return res.send(req.query.echostr);
+	} else {
+		return res.send(req.query.echostr);
+//		return res.send({
+//			msg:"request error";
+//		});
+	}
+};
 
 exports.receiveMeassage = function(req,res,next){
-	console.log('请求成功~~~');
-	console.log(req.query.signature);
-	console.log(req.query.signature);
-	console.log(req.query.timestamp);
-	console.log(req.query.nonce);
-	console.log(req.query.echostr);
-	res.send(req.query.echostr);
-}
+	var message = req.body;
+	if (message.Event == "user_enter_tempsession") {
+		console.log(`${message.SessionFrom} comein customer-service section~`);
+	}
+	
+	console.log(JSON.stringify(message));
+	
+	return res.send('success');
+};
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var customerService = require('../controller/customer-service');
 
-router.get('/', customerService.receiveMeassage);
-
+router.get('/', customerService.verification);
+router.post('/', customerService.receiveMeassage);
+router.get('/sha1', customerService.sha1);
 module.exports = router;
